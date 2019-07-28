996e7fc57e0459f2cab7ca168c10321e89b92930
Sergio Zulueta <sergio.zulueta@ikasle.egibide.org>
2019-07-28 21:01:34 +0200

    Persona obj

---
+++ Proyecto/JAMS/UML/Persona.js
@@ -0,0 +1,15 @@
+class Persona{
+	
+	private Integer user_id;
+	private String first_name;
+	private Integer age;
+	private String gender;
+	private String about_me;
+	private String work;
+	private Integer age_range;
+	
+	
+	
+	
+	
+}
